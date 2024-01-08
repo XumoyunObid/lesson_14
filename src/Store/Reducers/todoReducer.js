16c6565fcd4f28a45f0cf452be8a5de062917be6
Xumoyun <obidjonov06122005@gmail.com>
2024-01-09 00:22:06 +0500

    ...

---
+++ src/Store/Reducers/todoReducer.js
@@ -0,0 +1,20 @@
+const initialState = [];
+
+function todoReducer(state = initialState, action) {
+  switch (action.type) {
+    case "CHECK_TODO":
+      return state.map((todo) =>
+      todo.id === action.payload
+        ? { ...todo, completed: !todo.completed }
+        : todo
+    );
+    case "ADD_TODO":
+      return [...state, action.payload];
+    case "DELETE_TODO":
+      return state.filter((todo) => todo.id !== action.payload);
+    default:
+      return state;
+  }
+}
+
+export default todoReducer;
